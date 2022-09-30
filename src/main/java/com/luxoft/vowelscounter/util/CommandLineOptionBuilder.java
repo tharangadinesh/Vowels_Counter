0058ff06ab9d089114b7f9de7dbdaf96ff5fc8f3
tharanga <tharanga@tharanga-Inspiron-3501>
2022-09-30 12:08:37 +0530

    re-factor command line option builder
    Signed-off-by: tharanga <tharanga@tharanga-Inspiron-3501>

--- src/main/java/com/luxoft/vowelscounter/util/CommandLineOptionBuilder.java
+++ src/main/java/com/luxoft/vowelscounter/util/CommandLineOptionBuilder.java
@@ -1,33 +1,40 @@
 package com.luxoft.vowelscounter.util;
 
+import java.util.Arrays;
+
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class CommandLineOptionBuilder {
 
+
 	public static final Option INPUT = new Option("i", "input", true, "Input");
 
-	private static CommandLine commandLine = null;
-
+	/**
+	 * "Parsing" stage of command-line processing demonstrated with
+	 * Apache Commons CLI.
+	 * 
+	 * @param arguments Command-line arguments provided to application.
+	 * @return Instance of CommandLine as parsed from the provided Options and
+	 *    command line arguments; may be {@code null} if there is an exception
+	 *    encountered while attempting to parse the command line options.
+	 */
 	public static CommandLine getCLIOptions (String arguments[]) {
 
-		if (commandLine == null) {
+		try {
 
-			CommandLineParser commandLineParser = new DefaultParser();
+			return new DefaultParser().parse(new Options().addOption(INPUT), arguments);
 
-			Options options = new Options();
-			options.addOption(INPUT);
-
-			try {
-				commandLine = commandLineParser.parse(options, arguments);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		} catch (ParseException parseException)  {
+			System.out.println(
+					"ERROR: Unable to parse command-line arguments "
+							+ Arrays.toString(arguments) + " due to: "
+							+ parseException);
 		}
 
-		return commandLine;
+		return null;
 	}
 }
